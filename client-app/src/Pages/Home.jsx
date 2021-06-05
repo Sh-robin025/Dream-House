70588a4535671230c7492e39ec8f033f2acb3612
Shahadat Robin <sh.robin025@gmail.com>
2021-06-05 20:34:30 +0600

    conflict removed

--- client-app/src/Pages/Home.jsx
+++ client-app/src/Pages/Home.jsx
@@ -1,23 +1,4 @@
-<<<<<<< HEAD
-import React from 'react';
-<<<<<<< HEAD
-=======
-import Footer from "../components/Footer/Footer";
->>>>>>> 8d17d73cafb9abbe1975ae44fc9c600a38976f2e
-import HeroSection from "../components/HeroSection/HeroSection";
 
-const Home = () => {
-    return (
-      <div>
-        {/* <h1>This is home page</h1> */}
-        <HeroSection></HeroSection>
-<<<<<<< HEAD
-=======
-        <Footer></Footer>
->>>>>>> 8d17d73cafb9abbe1975ae44fc9c600a38976f2e
-      </div>
-    );
-=======
 import React from "react";
 import Footer from "../components/Footer/Footer";
 import HeroSection from "../components/HeroSection/HeroSection";
@@ -30,7 +11,6 @@ const Home = () => {
       <Footer></Footer>
     </div>
   );
->>>>>>> 355921693fc3b8aca473ffa7fb768bab71b26d51
 };
 
 export default Home;
--- client-app/src/components/HeroSection/HeroSection.jsx
+++ client-app/src/components/HeroSection/HeroSection.jsx
@@ -1,22 +1,8 @@
 import React from "react";
-<<<<<<< HEAD
-<<<<<<< HEAD
+import "./HeroStyle.css";
 
 const HeroSection = () => {
   return (
-    <div>
-      <h1>test</h1>
-=======
-// import HeaderStyle from "../../Styles/HomePage/HeroSection";
-import "./HeroStyle.css"
-const HeroSection = () => {
-  return (
-=======
-// import HeaderStyle from "../../Styles/HomePage/HeroSection";
-import "./HeroStyle.css";
-const HeroSection = () => {
-  return (
->>>>>>> 355921693fc3b8aca473ffa7fb768bab71b26d51
     <div id="HeroSection">
       <div className="HeroBGOverlay"></div>
 
@@ -37,46 +23,8 @@ const HeroSection = () => {
           </div>
         </form>
       </div>
-<<<<<<< HEAD
->>>>>>> 8d17d73cafb9abbe1975ae44fc9c600a38976f2e
-=======
->>>>>>> 355921693fc3b8aca473ffa7fb768bab71b26d51
     </div>
   );
 };
 
 export default HeroSection;
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 355921693fc3b8aca473ffa7fb768bab71b26d51
-
-{
-  /* <div className="HeroText">
-        <h1 className="text-center heroHeadline">FIND YOUR HOUSE RENT</h1>
-        <form action="">
-          <div className="heroSearch row">
-            <div className="col-md-8">
-              <input
-                className="form-control"
-                type="search"
-                placeholder="Search..."
-                name=""
-                id=""
-              />
-            </div>
-            <div className="col-md-4">
-              <button className="HeroSearchBtn" value="search">
-                Find Now
-              </button>
-            </div>
-          </div>
-          <div className=" d-flex"></div>
-        </form>
-      </div> */
-}
-<<<<<<< HEAD
->>>>>>> 8d17d73cafb9abbe1975ae44fc9c600a38976f2e
-=======
->>>>>>> 355921693fc3b8aca473ffa7fb768bab71b26d51
